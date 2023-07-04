73bface3316e34e9cf5550de71d38e2c55c37d68
akezeth <116703083+akezeth@users.noreply.github.com>
2023-07-04 13:32:40 +0300

    Fetch data from Api

--- src/redux/cryptocurrency/cryptoSlice.js
+++ src/redux/cryptocurrency/cryptoSlice.js
@@ -1,16 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  cryptoList: [],
-  isLoading: false,
+  crytoList: [],
+  isloading: false,
   error: '',
 };
 
-const cryptoSlice = createSlice({
-  name: 'cryptoList',
-  initialState,
-  reducers: {
+const url = 'https://api.coincap.io/v2/assets';
 
+export const fetchCrypto = createAsyncThunk('crypto/fetchCrypto', async (thunkAPI) => {
+  try {
+    const response = await axios(url);
+    return response.data.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('something went wrong!');
+  }
+});
+
+const cryptoSlice = createSlice({
+  name: 'crytoList',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCrypto.pending, (state) => ({
+      ...state, isloading: true,
+    }));
+    builder.addCase(fetchCrypto.fulfilled, (state, action) => ({
+      ...state,
+      isloading: false,
+      crytoList: action.payload,
+      error: '',
+    }));
+
+    builder.addCase(fetchCrypto.rejected, (state, action) => ({
+      ...state,
+      isloading: false,
+      crytoList: [],
+      error: action.error.message,
+    }));
   },
 });
 
