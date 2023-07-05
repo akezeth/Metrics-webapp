cef2eee2597d636a8bbd7231cf2b43285bc10c75
akezeth <116703083+akezeth@users.noreply.github.com>
2023-07-05 14:50:35 +0300

    Update app title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Details from './components/Details';
 function App() {
   return (
     <div className="App">
-      <h1 className="appTitle">CryptoCurrency</h1>
+      <h1 className="appTitle">CryptoCurrency Tracker</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<CryptoCurrency />} />
